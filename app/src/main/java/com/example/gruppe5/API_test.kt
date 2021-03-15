bbab41444ecd8a18b43b3f3133556e542cf3f329
borgebj <57920815+borgebj@users.noreply.github.com>
2021-03-15 12:14:18 +0100

    Endret API-test

--- app/src/main/java/com/example/gruppe5/API_test.kt
+++ app/src/main/java/com/example/gruppe5/API_test.kt
@@ -2,10 +2,29 @@ package com.example.gruppe5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class API_test : AppCompatActivity() {
+
+    lateinit var text_en: TextView
+    lateinit var text_to: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a_p_i_test)
+
+        assignId()
+        hentInfo()
+    }
+
+    fun assignId() {
+        text_en = findViewById(R.id.textView)
+        text_to = findViewById(R.id.textView2)
+    }
+
+    fun hentInfo() {
+        
     }
 }
