8bed55b60531ba6cc218d99c7ce9d8a390483967
borgebj <borgebj@uio.no>
2021-03-17 19:07:15 +0100

    Tester stasjon

--- app/src/main/java/com/example/gruppe5/API_test.kt
+++ app/src/main/java/com/example/gruppe5/API_test.kt
@@ -3,18 +3,22 @@ package com.example.gruppe5
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class API_test : AppCompatActivity() {
 
-    lateinit var listView: ListView
+    lateinit var recycler: RecyclerView
+    lateinit var adapter: StasjonAdapter
     lateinit var button: Button
     var stasjoner: MutableList<Stasjon> = mutableListOf()
     val baseURL: String = "https://api.met.no/weatherapi/airqualityforecast/0.1"
@@ -25,15 +29,25 @@ class API_test : AppCompatActivity() {
         setContentView(R.layout.activity_a_p_i_test)
 
         assignId()
+        addAdapter()
 
+        // naar knappen klikkes
         button.setOnClickListener {
             hentInfo()
         }
     }
 
+    // assigner ID'er
     fun assignId() {
-        listView = findViewById(R.id.listView)
+        recycler = findViewById(R.id.recycler)
         button = findViewById(R.id.button2)
+        recycler.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+    }
+
+    // legger til adapter til RecyclerView
+    fun addAdapter() {
+        adapter = StasjonAdapter(stasjoner)
+        recycler.adapter = adapter
     }
 
     // henter JSON/XML via KHTTP -> til String
@@ -50,12 +64,16 @@ class API_test : AppCompatActivity() {
             val response = getData("/stations")
             val gson = Gson()
 
-            val listPersonType = object : TypeToken<List<Stasjon>>() {}.type
-            var station: List<Stasjon> = gson.fromJson(response, listPersonType)
-
-            for (hver in station) {
-                Log.d("Stasjon:", hver.toString())
+            try {
+                val test = JSONArray(response)
+            } catch (e: JSONException) {
+                e.printStackTrace()
             }
+
+            //val listPersonType = object : TypeToken<List<Stasjon>>() {}.type
+            //val station: List<Stasjon> = gson.fromJson(response, listPersonType)
+            //val list: Array<Stasjon> = gson.fromJson(response, Array<Stasjon>::class.java)
+
         } //endregion
     }
 }
---
+++ app/src/main/java/com/example/gruppe5/StasjonAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.gruppe5
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class StasjonAdapter(private val liste: MutableList<Stasjon>) :
+        RecyclerView.Adapter<StasjonAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textEn: TextView = view.findViewById(R.id.textEn)
+        val textTo: TextView = view.findViewById(R.id.textTo)
+        var textTre: TextView = view.findViewById(R.id.textTre)
+        var textFire: TextView = view.findViewById(R.id.textTre)
+    }
+
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.element, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.textEn.text = "Name: ${liste[position]}"
+        //viewHolder.textTo.text = "eoi: ${liste[position].eoi}"
+        //viewHolder.textTre.text = "Height: ${liste[position].height}"
+        //viewHolder.textFire.text = "Longitude: ${liste[position].longitude}"
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = liste.size
+}
