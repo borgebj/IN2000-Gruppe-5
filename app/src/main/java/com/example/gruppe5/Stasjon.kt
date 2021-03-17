6793faaa193f3febedc15be73eb1fffc80f35e01
borgebj <borgebj@uio.no>
2021-03-17 12:42:08 +0100

    API-test endringer
    Skriver ut hver stasjon nå

--- app/src/main/java/com/example/gruppe5/Stasjon.kt
+++ app/src/main/java/com/example/gruppe5/Stasjon.kt
@@ -3,13 +3,13 @@ package com.example.gruppe5
 class Stasjon {
 
     data class StasjonInfo(
-    val name: String,
-    val eoi: String,
-    val height: Int,
-    val longitude: Double,
-    val grunnkrets: Grunnkets,
-    val delomrade: Delomrade,
-    val kommune: Kommune
+        val name: String,
+        val eoi: String,
+        val height: Int,
+        val longitude: Double,
+        val grunnkrets: Grunnkets,
+        val delomrade: Delomrade,
+        val kommune: Kommune
     )
 
     data class Grunnkets(
@@ -26,4 +26,8 @@ class Stasjon {
         val name: String,
         val areacode: Int
     )
+
+    override fun toString(): String {
+        return "${Grunnkets}"
+    }
 }
