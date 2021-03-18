e906b5b5a0330b2eebd7cdf4b239635abf1fda22
birgiryg <birgiryg@ifi.uio.no>
2021-03-18 17:20:43 +0100

    Lagt til Fragment og ViewModel for Home, Map, Info og Profile utifrå MVVM. Skal brukast ilag med navigasjonsbar.

---
+++ app/src/main/java/com/example/gruppe5/ui/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.gruppe5.ui.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+}
---
+++ app/src/main/java/com/example/gruppe5/ui/info/InfoFragment.kt
@@ -0,0 +1,9 @@
+package com.example.gruppe5.ui.info
+
+import androidx.fragment.app.Fragment
+
+class InfoFragment : Fragment() {
+
+    private lateinit var infoViewModel: InfoViewModel
+
+}
