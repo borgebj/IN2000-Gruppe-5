1a0fe98aafe96abf7f5ad0b91e3ec4c12cc9330a
borgebj <borgebj@uio.no>
2021-03-22 12:06:13 +0100

    Tilgang til kart

---
+++ app/src/main/java/com/example/gruppe5/testFiler/FirstFragment.kt
@@ -0,0 +1,4 @@
+package com.example.gruppe5.testFiler
+
+class FirstFragment {
+}
---
+++ app/src/main/java/com/example/gruppe5/testFiler/SecondFragment.kt
@@ -0,0 +1,6 @@
+package com.example.gruppe5.testFiler
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment() {
+}
