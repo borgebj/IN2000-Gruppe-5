06803e8a23aff274eb52d48e20d656ab00a1f3fd
borgebj <borgebj@uio.no>
2021-03-22 05:05:16 +0100

    Optimisering + rydding i kode
    Delt inn testkode og mainkode i ulike deler

--- app/src/main/java/com/example/gruppe5/MainActivity.kt
+++ app/src/main/java/com/example/gruppe5/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.gruppe5
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.gruppe5.testFiler.MainTestActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mapTest: Button
-    lateinit var api_test: Button
+    lateinit var tester: Button
     lateinit var navView: BottomNavigationView
     lateinit var textView: TextView
 
@@ -25,33 +26,23 @@ class MainActivity : AppCompatActivity() {
         assignId()
         setOnClickers()
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        navView.setupWithNavController(navController)
+        // setter opp navbar
+        navView.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 
     fun assignId() {
         navView = findViewById(R.id.nav_view)
-        api_test = findViewById(R.id.button1)
-        mapTest = findViewById(R.id.button2)
         textView = findViewById(R.id.mainText_en)
+        tester = findViewById(R.id.testFiler_knapp)
     }
 
     // setter onClickers for kart og API_test
     fun setOnClickers(){
-        // gaar til mapTest-siden
-        mapTest.setOnClickListener {
-            val map = Intent(this, MapTest::class.java)
-            startActivity(map)
-        }
 
-        // gaar til API_test-siden
-        api_test.setOnClickListener {
-            val api = Intent(this, API_test::class.java)
-            startActivity(api)
+        // gaar til test-filene
+        tester.setOnClickListener {
+            val intent = Intent(this, MainTestActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/gruppe5/testFiler/FragmentTest.kt
@@ -0,0 +1,14 @@
+package com.example.gruppe5.testFiler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gruppe5.R
+
+class FragmentTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_test)
+        findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbarMain).title = "Fragment-test"
+
+    }
+}
