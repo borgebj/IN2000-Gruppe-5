0723e8f10363843db4b206c575b8b778f69202ac
borgebj <borgebj@uio.no>
2021-03-31 00:56:29 +0200

    Update til stasjonsverdier
    Kan nå velge type forurensning å vise. Stasjon også. Så det skal gå greit å hente luftverdier nå ihvertfall.

--- app/src/main/java/com/example/gruppe5/testFiler/StasjonVerdier.kt
+++ app/src/main/java/com/example/gruppe5/testFiler/StasjonVerdier.kt
@@ -1,11 +1,9 @@
 package com.example.gruppe5.testFiler
 
-import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.annotation.RequiresApi
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gruppe5.R
 import kotlinx.coroutines.CoroutineScope
@@ -14,44 +12,53 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.json.JSONObject
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class StasjonVerdier : AppCompatActivity() {
+class StasjonVerdier : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     lateinit var boks : TextView
     lateinit var input : EditText
     lateinit var submit : Button
+    lateinit var spinner: Spinner
     val baseURL: String = "https://api.met.no/weatherapi/airqualityforecast/0.1"
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'") // tidsformat brukt i API og prosjekt
+
+    // testvariabler ! Disse kan endres!
+    var stasjon = "NO0011A"
+    val reftime = "2021-03-28T12%3A00%3A00Z"
+    var type = ""
 
 
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stasjon_verdier)
 
-        val string = "2018-04-10T04:00:00.000Z"
-        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-        val date: LocalDate = LocalDate.parse(string, formatter)
-        println(date)
 
-
-        val kirkeveien = "NO0011A"
-        val reftime = "2021-03-28T12%3A00%3A00Z"
+        // test-dato og naa-dato
         val klokkeslett = "2021-03-30T16:00:00Z"
-        val type = "o3"
+        val idag = dateFormat.format(Calendar.getInstance().time)
+
 
         assignId()
-        parseJson(kirkeveien, reftime, klokkeslett, type)
-    }
+        addSpinnerAdapter()
 
+        // parser
+        parseJson(reftime, idag)
+    }
 
     fun assignId() {
         boks = findViewById(R.id.verdiBoks)
         input = findViewById(R.id.verdiInput)
         submit = findViewById(R.id.submit)
+        spinner = findViewById(R.id.spinner)
+    }
+    fun addSpinnerAdapter() {
+        ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = it }
+        spinner.onItemSelectedListener = this
     }
 
     // henter JSON/XML via KHTTP -> til String
@@ -60,11 +67,9 @@ class StasjonVerdier : AppCompatActivity() {
         return khttp.get(full).text
     }
 
-    fun parseJson(stasjon: String, refTime: String, klokkeslett: String, type: String) {
+    fun parseJson(refTime: String, klokkeslett: String) {
         submit.setOnClickListener {
-
-            var fullString = ""
-
+            if (input.text.toString() != "") stasjon = input.text.toString()
             CoroutineScope(Dispatchers.IO).launch {
 
                 // linken med spesifikk stasjon og tidspunkt (Kan og maa modifiseres !)
@@ -86,24 +91,38 @@ class StasjonVerdier : AppCompatActivity() {
                     val pm25 = variabler.getJSONObject("AQI_pm25")
                     val o3 = variabler.getJSONObject("AQI_o3")
 
+                    // henter og splitter tidspunkt fra i dag
+                    val idagListe = klokkeslett.split("T")
+                    val idagDato = idagListe[0]
+                    val idagKlokke = idagListe[1]
+
+                    // henter og splitter tidspunkter fra api-tidspunktene
+                    val apiList = tidspunkt.get("from").toString().split("T")
+                    val apiDato = apiList[0]
+                    val apiKlokke = apiList[1]
+
                     // sammenligner gitt tidspunkt og type
-                    if (tidspunkt.get("from") == klokkeslett) {
-                        when(type) {
-                            "no2" -> fullString = "${tidspunkt.get("from")} - $no2"
-                            "pm10" -> fullString = "${tidspunkt.get("from")} - $pm10"
-                            "pm25" -> fullString = "${tidspunkt.get("from")} - $pm25"
-                            "o3" -> fullString = "${tidspunkt.get("from")} - $o3"
+                    if (apiDato == idagDato && apiKlokke <= idagKlokke) {
+                        withContext(Dispatchers.Main) {
+                            when (type) {
+                                "no2" -> boks.text = "${stasjon} \n ${tidspunkt.get("from")} \n no2: ${"%.4f".format(no2.get("value"))}"
+                                "pm10" -> boks.text = "${stasjon} \n ${tidspunkt.get("from")} \n pm10: ${"%.4f".format(pm10.get("value"))}"
+                                "pm25" -> boks.text = "${stasjon} \n ${tidspunkt.get("from")} \n pm25: ${"%.4f".format(pm25.get("value"))}"
+                                "o3" -> boks.text = "${stasjon} \n ${tidspunkt.get("from")} \n o3: ${"%.4f".format(o3.get("value"))}"
+                            }
                         }
                     }
-                }
-
-                // endrer teksten i boksen
-                withContext(Dispatchers.Main) {
-                    boks.text = fullString
                 }
             }
         }
     }
+
+    // endrer typen ved item-selected
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        type = parent.getItemAtPosition(position).toString()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 }
 
 // Teststasjoner:
