b390fae90b56b8943725e19d4984dba3356e2a80
Rie <rieny@uio.no>
2021-04-01 13:33:22 +0200

    Tester å legge til en by

--- app/src/main/java/com/example/gruppe5/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/gruppe5/ui/favorites/FavoritesFragment.kt
@@ -5,18 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gruppe5.R
 import com.example.gruppe5.Stasjon
 import com.example.gruppe5.testFiler.StasjonAdapter
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.coroutines.awaitString
 import com.google.gson.Gson
 import kotlinx.coroutines.*
 
@@ -27,10 +28,13 @@ class FavoritesFragment : Fragment() {
     lateinit var textView: TextView
     lateinit var searchBar: EditText
     lateinit var searchBut: ImageButton
-    lateinit var recycler: RecyclerView
-    lateinit var adapter: StasjonAdapter // gjenbruker StasjonAdapter fra testFiler
+    lateinit var fav_recycler: RecyclerView
+    lateinit var fav_adapter: StasjonAdapter // gjenbruker StasjonAdapter fra testFiler
 
-    private val path: String = "https://in2000-apiproxy.ifi.uio.no/weatherapi/airqualityforecast/0.1/stations"
+    lateinit var stasjoner : List<Stasjon>
+
+    private val path: String = "https://api.met.no/weatherapi/airqualityforecast/0.1/stations"
+        //"https://in2000-apiproxy.ifi.uio.no/weatherapi/airqualityforecast/0.1/stations"
 
     // maa beholde denne listen for brukeren paa en eller annen maate -- kanskje Model?????
     var fav_stations: MutableList<Stasjon> = mutableListOf()
@@ -44,38 +48,52 @@ class FavoritesFragment : Fragment() {
 
         var wanted : String
 
-        suspend fun getData() : List<Stasjon> {
-
-            var ret : List<Stasjon> = listOf()
-            try {
-                ret = gson.fromJson(Fuel.get(path).awaitString(), Array<Stasjon>::class.java).toList()
-            }
-            catch (exception: Exception) {
-                println("A network request exception was thrown: ${exception.message}")
-            }
-
-            return ret
+        fun getData(): String {
+            return khttp.get(path).text
         }
 
         searchBut.setOnClickListener {
 
-            wanted = searchBar.text.toString()
-
-            val data = "https://in2000-apiproxy.ifi.uio.no/weatherapi/airqualityforecast/0.1/stations"
+            wanted = searchBar.text.toString() // henter det brukeren tastet inn
+            Log.d("WANTED", wanted)
 
             CoroutineScope(Dispatchers.IO).launch {
 
-                val response = getData()//gson.fromJson(Fuel.get(data).awaitString(), Array<Stasjon>::class.java)//getData(data)
+                stasjoner = gson.fromJson(getData(), Array<Stasjon>::class.java).toList()
+                    //gson.fromJson(Fuel.get(data).awaitString(), Array<Stasjon>::class.java)//getData(data)
 
                 withContext(Dispatchers.Main){
-                    Log.d("API FETCHING", response.toString())
-                }
+                    Log.d("API FETCHING", stasjoner.toString())
 
+                    if (wanted != "") {
+                        var added = false
+                        for (station in stasjoner){
+                            Log.d("I FOR_LOEKKE", station.name)
+
+                            if (station.name.equals(wanted, ignoreCase = true)){
+                                if (station in fav_stations) { // unngaa duplikasjon
+                                    toastMsg("${wanted} is already your favorite city.")
+                                    return@withContext
+                                }
+                                else {
+                                    fav_stations.add(station)
+                                    fav_adapter.notifyDataSetChanged()
+                                    Log.d("FANT STASJON", station.name)
+                                    added = true
+                                }
+                            }
+                        }
+                        if (!added) toastMsg("${wanted} does not exsist.")
+
+                    }
+                    else toastMsg("Enter a city name")
+
+                    searchBar.text.clear()
+                    //searchBar.onEditorAction(EditorInfo.IME_ACTION_DONE) // lukker IKKE tastatur
+                }
             }
 
-
         }
-
 
         return root
     }
@@ -92,53 +110,31 @@ class FavoritesFragment : Fragment() {
         textView = root.findViewById(R.id.text_favorites)
         searchBar = root.findViewById(R.id.search_bar)
         searchBut = root.findViewById(R.id.search_but)
-        recycler = root.findViewById(R.id.favorites_recycler)
+        fav_recycler = root.findViewById(R.id.favorites_recycler)
+        fav_recycler.layoutManager = LinearLayoutManager(root.context, RecyclerView.VERTICAL, false)
     }
 
     fun addAdapter() {
-        adapter = StasjonAdapter(fav_stations)
-        recycler.adapter = adapter
+        fav_adapter = StasjonAdapter(fav_stations)
+        fav_recycler.adapter = fav_adapter
+    }
+
+    fun toastMsg(msg : String){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 
 
-    private fun setOnClickers() {
+    /*suspend fun getData() : List<Stasjon> { // Fuel. funksjonen funker kun med proxy-serveren. funksjonen settes i onCreateView()
 
-        var wanted : String
-
-        searchBut.setOnClickListener {
-
-            wanted = searchBar.text.toString()
-
-
-            CoroutineScope(Dispatchers.IO).launch {
-
-                val response = gson.fromJson(Fuel.get(path).awaitString(), Array<Stasjon>::class.java).toList()
-
-                Log.d("ETTER API FETCHING", path)
-                Log.d("API FETCHING", response.toString())
-
-            }
-
-
-
-            //    gson.fromJson(Fuel.get(distriktAdr).awaitString(), Array<Candidate>::class.java).toList()
-
-            //henter info om "wanted" (station) med API-fetching
-
-            //if API success
-            //    --> legge til "wanted" i lista, fav_stations
-            // data vi fikk tilbake fra API-fetching
-
-            //    fav_stations.add(response)
-
-
-
-            //else
-            //    --> "Station not found"-melding til brukeren
-
-
-
+        var ret : List<Stasjon> = listOf()
+        try {
+            ret = gson.fromJson(Fuel.get(path).awaitString(), Array<Stasjon>::class.java).toList()
         }
-    }
+        catch (exception: Exception) {
+            println("A network request exception was thrown: ${exception.message}")
+        }
+
+        return ret
+    }*/
 
 }
