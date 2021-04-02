419ffe06a74f6bbfde5b15fb735ac1f5f48a7087
BjornBirkelund <bbirkelund@gmail.com>
2021-04-02 20:01:43 +0200

    made donut work

--- app/src/main/java/com/example/gruppe5/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gruppe5/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.gruppe5.ui.home
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import app.futured.donut.DonutProgressView
+import app.futured.donut.DonutSection
 import com.example.gruppe5.R
 import com.example.gruppe5.testFiler.MainTestActivity
 
@@ -18,14 +21,34 @@ class HomeFragment : Fragment() {
 
     // globale variabler
     private lateinit var homeModel: HomeViewModel
+    lateinit var donutView: DonutProgressView
     lateinit var textView: TextView
     lateinit var testFilKnapp: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root: View = inflater.inflate(R.layout.fragment_home, container, false)
-
+        val section1 = DonutSection(
+            name = "normal_pollution",
+            color = Color.parseColor("#FF46E33B"),
+            amount = 60f
+        )
+        val section2 = DonutSection(
+            name = "warning_pollution",
+            color = Color.parseColor("#FFDDE33B"),
+            amount = 20f
+        )
+        val section3 = DonutSection(
+            name = "dangerous_pollution",
+            color = Color.parseColor("#FFE33B3B"),
+            amount = 10f
+        )
         assignId(root)
         setOnClickers(root)
+
+        donutView.cap = 100f
+        donutView.submitData(listOf(section1,section2,section3))
+
+
 
         return root
     }
@@ -41,12 +64,13 @@ class HomeFragment : Fragment() {
     fun assignId(root: View) {
         textView = root.findViewById(R.id.text_home)
         testFilKnapp = root.findViewById(R.id.testFiler_knapp)
+        donutView = root.findViewById(R.id.donut_view)
     }
 
     // setter onClickers for kart og API_test
     fun setOnClickers(root: View){
 
-        // gaar til map-tester
+//         gaar til map-tester
         testFilKnapp.setOnClickListener {
             val map = Intent(root.context, MainTestActivity::class.java)
             startActivity(map)
