c4e4239bbc3d5d3dceade9f1ed9dedf47e4f7e56
Rie <rieny@uio.no>
2021-04-14 13:04:40 +0200

    Fikset toolbar

--- app/src/main/java/com/example/gruppe5/MainActivity.kt
+++ app/src/main/java/com/example/gruppe5/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -16,10 +18,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        assignId()
+        //assignId()
+        navView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_map, R.id.navigation_favorites, R.id.navigation_settings))
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         // setter opp navbar
-        navView.setupWithNavController(findNavController(R.id.nav_host_fragment))
+        navView.setupWithNavController(navController)
 
 
     }
