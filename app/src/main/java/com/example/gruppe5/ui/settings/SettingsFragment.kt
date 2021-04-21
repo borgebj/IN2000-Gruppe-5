cbafa2d4a665ba75de76c275b2db699bdce15809
birgiryg <birgiryg@ifi.uio.no>
2021-04-21 09:25:34 +0200

    Lagt til lokasjons-knapp

--- app/src/main/java/com/example/gruppe5/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/gruppe5/ui/settings/SettingsFragment.kt
@@ -1,33 +1,30 @@
 package com.example.gruppe5.ui.settings
 
-import android.annotation.SuppressLint
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import android.text.Layout
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Switch
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import com.example.gruppe5.R
-import com.example.gruppe5.testFiler.MainTestActivity
-import com.example.gruppe5.ui.favorites.FavoritesFragmentDirections
+import com.google.android.gms.maps.GoogleMap
 
 
 class SettingsFragment : Fragment() {
 
-
     private lateinit var settingsModel: SettingsViewModel
+
     private lateinit var innstillinger: TextView
-
-    private lateinit var lokasjon: Button
-
+    private lateinit var lokasjonKnapp: Button
     private lateinit var informasjon: TextView
     private lateinit var luftkvalitetKnapp: Button
     private lateinit var appKnapp: Button
@@ -42,7 +39,8 @@ class SettingsFragment : Fragment() {
         val root: View = inflater.inflate(R.layout.fragment_settings, container, false)
 
         assignId(root)
-        setLokasjon()
+        setTilstand(root)
+        setLokasjon(root)
         setOmLuftkvalitet(root)
 
         return root
@@ -58,19 +56,57 @@ class SettingsFragment : Fragment() {
 
     fun assignId(root: View) {
         innstillinger = root.findViewById(R.id.innstillinger)
-        lokasjon = root.findViewById(R.id.location_button)
+        lokasjonKnapp = root.findViewById(R.id.location_button)
         informasjon = root.findViewById(R.id.informasjon)
         luftkvalitetKnapp = root.findViewById(R.id.button1)
         appKnapp = root.findViewById(R.id.button2)
         dataKnapp = root.findViewById(R.id.button3)
-
     }
 
-    fun setLokasjon() {
-        lokasjon.setOnClickListener{
 
+    fun setLokasjon(root: View) {
+        lokasjonKnapp.setOnClickListener{
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            setTilstand(root)
         }
     }
+
+
+    fun setTilstand(root: View) {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (location) {
+            lokasjonKnapp.setText("På")
+        } else {
+            lokasjonKnapp.setText("Av")
+        }
+    }
+
+        /*
+        val mMap : GoogleMap = root.findViewById(R.id.map)
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (location) {
+            lokasjonKnapp.setText("På")
+            mMap.uiSettings.isMyLocationButtonEnabled = true
+            if (ActivityCompat.checkSelfPermission(
+                    root.context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    root.context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            mMap.isMyLocationEnabled = true
+        } else {
+            lokasjonKnapp.setText("Av")
+            mMap.uiSettings.isMyLocationButtonEnabled = false
+            mMap.isMyLocationEnabled = false
+        }
+        */
+
 
     fun setOmLuftkvalitet(root: View) {
         luftkvalitetKnapp.setOnClickListener() {
