3234ff7df6aadf77971c41ac056bc7a4f5cb3458
jonaslpe <jonaslpe@users.noreply.github.com>
2021-04-26 18:13:14 +0200

    Fikset animasjoner og tekst i dialog

--- app/src/main/java/com/example/gruppe5/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gruppe5/ui/home/HomeFragment.kt
@@ -74,45 +74,57 @@ class HomeFragment : Fragment(){
         //infoknapp
         val infoButton : ImageButton = root.findViewById(R.id.info_home)
         infoButton.setOnClickListener{
-            alertView(getString(R.string.str_info), root)
+            alertView(getString(R.string.str_info), root, "open")
         }
 
         //Test for Location, da slipper jeg å accesse Locations gjennom mappet hele tiden
         val locButton : ImageButton = root.findViewById(R.id.iconLocation_home)
         locButton.setOnClickListener{
-            alertValuesView(getString(R.string.str_info_values))
+            alertValuesView(getString(R.string.str_info_values), "open")
         }
     }
 
+    private fun slideShow(command : String, dialog : AlertDialog.Builder){
+        val animasjonsDialog : AlertDialog = dialog.create()
+        if (command == "open") animasjonsDialog.window?.attributes?.windowAnimations = R.style.DialogThOpen //animasjon
+        else if (command == "close") animasjonsDialog.window?.attributes?.windowAnimations = R.style.DialogThClose //animasjon
+        else if (command == "next") animasjonsDialog.window?.attributes?.windowAnimations = R.style.DialogThNext //animasjon
+        else if (command == "nextNext") animasjonsDialog.window?.attributes?.windowAnimations = R.style.DialogThNext //animasjon
+        else if (command == "back") animasjonsDialog.window?.attributes?.windowAnimations = R.style.DialogThBack
+        return (animasjonsDialog.show())
+    }
+
     //test for Location sin infoknapp om ulike nivåer. Gir en liste man kan velge i.
-    private fun alertValuesView(message: String) {
+    private fun alertValuesView(message: String, command : String) {
         val dialog = AlertDialog.Builder(context)
 
         dialog.setTitle("AQI nivåer")
             .setIcon(R.drawable.ic_info)
             .setMessage(message)
-            .setPositiveButton("Lukk") { dialoginterface, i -> }
-            .setNeutralButton("les mer") { dialog, which -> openValueList() }
-        dialog.show()
+            .setPositiveButton("Lukk") { dialoginterface, i -> } //legge til animasjon senere
+            .setNeutralButton("les mer") { dialog, which -> openValueList("next") }
+        //dialog.show()
+        slideShow(command, dialog)
+
     }
 
-    //kalles i metoden ovenfor. Åpner liste med ulike verdier, som man kan velge i for deretter å få en forklaring.
-    private fun openValueList(){
+    //kalles i metoden ovenfor. Åpner liste med ulike verdier, som man kan velge i for deretter å få en forklaring. åpnes når det trykkes "les mer"
+    private fun openValueList(command : String){
         // setter opp alert builder
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Velg en type")
+            .setNeutralButton("Tilbake") { dialog, which -> alertValuesView(getString(R.string.str_info_values), "back")}
 
-        val values = arrayOf("no2", "pm10", "pm25", "o3")
+        val values = arrayOf("NO2", "PM10", "PM2,5", "O3")
         builder.setItems(values) { dialog, which ->
             when (which) {
-                0 -> {displayTypeFact("no2 er ikke bra for lungene")}           //disse utdypes senere
-                1 -> {displayTypeFact("pm10 er ikke bra for lungene")}
-                2 -> {displayTypeFact("pm25 er ikke bra for lungene")}
-                3 -> {displayTypeFact("o3 er heller ikke bra for lungene")}
+                0 -> {displayTypeFact("NO2 kan være helseskadelig for alle mennesker, men barn, eldre og folk med luftveis- og hjertekar problemer er spesielt sårbare. \nNitrogendioksid (NO2) er en helseskadelig gass, og hovedkilden er trafikkerte veier. Helseeffekter er svekket lungeinfeksjon, og forsterkelse av astma. Langvarig eksponering kan bidra til utvikling av luftveissykdommer som astma.\nKalde vinterdager med lite vind, er dager som oftest blir vi utsatt for de høyeste konsentrasjonene om vinteren på kalde dager med lite vind, og spesielt på trafikkerte veier og i tunneler. Oslo og Bergen har hatt de høyeste verdiene." )}           //https://www.fhi.no/nyheter/2020/nitrogendioksid-forverrer-helsa-ved-lave-nivaer/
+                1 -> {displayTypeFact("PM10 er betegnelse på partikler med diameter under 10 mikrometer (1/1000 000 meter), og omtales i dagligtalen som svevestøv. Partiklene kan stamme fra blant annet industriutslipp og biltrafikk. Verdier over 35 mikrogram regnes som uakseptabelt ifølge vedtatte norske luftkvalitetskriterier. Ifølge Verdens helseorganisasjon (WHO) vil en tredagers periode med 50 mikrogram PM10 per kubikkmeter resultere i 1000 nye astmaanfall og fire dødsfall i en by med 1 million innbyggere. I England er det beregnet at PM10-partikler forårsaker 2000 til 10 000 dødsfall per år. Omlag 86 % av PM10 kommer fra vei- og gatetrafikk. I USA skyldes 64 000 dødsfall årlig virkninger på hjerte/lunge av svevestøv. Partiklene inneholder substanser som man vet er kreftfremkallende i andre sammenhenger.")}
+                2 -> {displayTypeFact("pm2,5 er betegnelse på partikler med diameter under 2,5 mikrometer, og omtales som fint svevestøv. Partiklene stammer hovedsakelig fra industriutslipp og biltrafikk. Siden de er så små og lette, har fine partikler en tendens til å holde seg lenger i luften enn tyngre partikler. Dette øker sjansene for at mennesker og dyr inhalerer partiklene. Barn, eldre, og de som lider av lunge- og / eller hjertesykdom er spesielt sårbare, og bør ta spesielle forholdsregler når PM2.5 verdien krysser usunne nivåer.")}
+                3 -> {displayTypeFact("o3 (Ozon) er en reaktiv gass som finnes både nær bakken og høyere opp i atmosfæren. Høye konsentrasjoner av bakkenært ozon i Norge skyldes hovedsakelig langtransportert ozon fra Europa. Ozon frigjøres ikke fra en primær kilde, men dannes via en rekke komplekse reaksjoner i luften. Konsentrasjonen av ozon er noe høyere utenfor byene enn i byene. Ozonkonsentrasjonen i Norge har episodevis nådd nivåer opp mot 160 μg/m3. Studier har vist at astmatiske barn kan få luftveissymptomer ved akutt eksponering for ozon fra 100 til 120 μg/m3. Ozon kan gi betennelse og føre til skader i luftveiene, samt svekke luftveisfunksjon og øke luftveisplager. Befolkningsstudier har vist sammenhenger mellom ozoneksponering og økt dødelighet av luftveis-, hjerte- og karsykdom, samt økt sykelighet for mennesker med luftveissykdommer.")} //https://www.fhi.no/nettpub/luftkvalitet/temakapitler/ozon/
             }
         }
-        val dialog = builder.create()
-        dialog.show()
+        slideShow(command, builder)
     }
 
     //for hver av typene i lista.
@@ -122,26 +134,29 @@ class HomeFragment : Fragment(){
             .setIcon(R.drawable.ic_info)
             .setMessage(message)
             .setPositiveButton("Lukk") { dialoginterface, i -> }
-            .setNeutralButton("Tilbake") { dialog, which -> openValueList() }
-        dialog.show()
+            .setNeutralButton("Tilbake") { dialog, which ->
+                openValueList("back")}
+        //dialog.show()
+        slideShow("next", dialog)
     }
 
 
     //viser dialog/pop up vindu. brukes for infoknapper
-    private fun alertView(message: String, root : View) {
-        val dialog = AlertDialog.Builder(context)
-        dialog.setTitle("Hva er AQI?")
+    private fun alertView(message: String, root : View, command : String) {
+        val dialogB = AlertDialog.Builder(context)
+        dialogB.setTitle("Hva er AQI?")
             .setIcon(R.drawable.ic_info)
             .setMessage(message)
-            .setPositiveButton("Lukk") { dialoginterface, i -> }
-            .setNeutralButton("les mer") { dialog, which -> openInfo(root)}
-            .setNegativeButton("se funfact") { dialog, which -> displayFunfacts(root)}
-        dialog.show()
-    }
-
-    //sender brukern til AboutAirQualityFragment når det trykkes "les mer"
-    private fun openInfo(root : View){
-        root.findNavController().navigate(R.id.action_navigation_dialog_to_AboutAirQualityFragment)
+            .setPositiveButton("Lukk") {
+                    dialoginterface,i -> }
+            .setNeutralButton("les mer") {
+                    dialog, which ->
+                root.findNavController().navigate(R.id.action_navigation_dialog_to_AboutAirQualityFragment)}
+            .setNegativeButton("se funfact") {
+                dialog, which ->
+                displayFunfacts(root)
+            }
+        slideShow(command, dialogB)
     }
 
     //viser fram fun facts etter man har trykket på infoknappen og "les funfacts"
@@ -158,17 +173,22 @@ class HomeFragment : Fragment(){
 
         var factIndex = (0 until facts.size).random()
         val newDialog = AlertDialog.Builder(context)
-        newDialog.setTitle("Funfacts om AQI")
+        newDialog.setTitle("Funfact om AQI")
         newDialog.setIcon(R.drawable.ic_funfact)
         newDialog.setMessage(facts.get(factIndex))
-            .setPositiveButton("Lukk", { dialoginterface, i -> })
-            .setNeutralButton("Neste funfact") { dialog, which ->
+            .setPositiveButton("Lukk") {
+                    dialoginterface, i ->}
+            .setNeutralButton("Neste funfact") {
+                dialog, which ->
                     factIndex = (0 until facts.size).random()
-                newDialog.setMessage(facts.get(factIndex))
-                newDialog.show()
+                    newDialog.setMessage(facts.get(factIndex))
+                    slideShow("next", newDialog)
             }
-            .setNegativeButton("tilbake") { dialog, which -> alertView(getString(R.string.str_info), root)}
-        newDialog.show()
+            .setNegativeButton("tilbake") {
+                dialog, which ->
+                    alertView(getString(R.string.str_info), root, "back")}
+        //newDialog.show()
+        slideShow("nextNext", newDialog)
     }
 
     //endrer dataen på hjemskjermen etter faktiske verdinivåer
